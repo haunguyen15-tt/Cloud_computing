a2e1b76e804093a3e26963142b7cfc9fe2dfc796
demons1502 <ngochautk123@gmail.com>
2022-05-16 16:35:52 +0700

    add User model

---
+++ models/userModel.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [
+      true,
+      'Please enter name of the user',
+    ],
+  },
+  email: {
+    type: String,
+    required: [
+      true,
+      'Please enter email address',
+    ],
+    unique: true,
+    lowcase: true,
+    validate: [
+      validator.isEmail,
+      'Please provide a valid email address',
+    ],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please enter password'],
+    minLength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [
+      true,
+      'Please confirm your password',
+    ],
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
