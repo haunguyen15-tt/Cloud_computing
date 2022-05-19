f2d8058f3efe40e92412f6e6c539532641b8b99a
demons1502 <ngochautk123@gmail.com>
2022-05-19 11:46:26 +0700

    add rate-limit

--- controller/authController.js
+++ controller/authController.js
@@ -13,6 +13,19 @@ const signToken = (id) =>
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    cookieOptions.secure = true;
+  }
+  res.cookie('jwt', token, cookieOptions);
+
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: 'success',
@@ -29,7 +42,6 @@ exports.signup = catchAsync(async (req, res, next) => {
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
-    role: req.body.role,
     passwordChangedAt: req.body.passwordChangedAt,
   });
   createSendToken(newUser, 201, res);
